c650b598efc21dde1ed74292ced8f18b82dce93d
Sulivan MICHEL <sulivan.michel@switchcompagnie.eu>
2024-01-14 20:16:27 +0100

    Footer Release

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,8 @@
-<h1>Made By SwitchCompagnie</h1>
+<footer>
+    <div class="footer-content">
+        <p>&copy; 2022 -
+            <?php echo date('Y'); ?> DigitalChocolateFrance. All rights reserved.
+        </p>
+        <p>Studios &amp; Offices: SwitchCompagnie, FRANCE</p>
+    </div>
+</footer>
