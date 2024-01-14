94e79c166a637073244fe6e8bad5bb5e4f0d8b41
Sulivan MICHEL <sulivan.michel@switchcompagnie.eu>
2024-01-14 18:59:28 +0100

    Play Button Fix Redirection

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,89 +1,93 @@
 <aside>
-    <div class="top">
-      <div class="logo">
-        <img class="logo" src="{{ asset('images/logo.svg') }}" alt="Logo" href="/" />
-        <h2>DCHOC <span class="danger">FR</span>
-        </h2>
-      </div>
-      <div class="close" id="close-btn">
-        <span class="material-symbols-sharp">close</span>
-      </div>
+  <div class="top">
+    <div class="logo">
+      <img class="logo" src="{{ asset('images/logo.svg') }}" alt="Logo" href="/" />
+      <h2>DCHOC <span class="danger">FR</span>
+      </h2>
     </div>
-    <div class="sidebar">
-      <button class="play-button rounded-corners"> Play <span class="vector-icon">
+    <div class="close" id="close-btn">
+      <span class="material-symbols-sharp">close</span>
+    </div>
+  </div>
+  <div class="sidebar">
+    <a href="{{ route('collections') }}">
+      <button class="play-button rounded-corners">
+        Play
+        <span class="vector-icon">
           <i class="fa fa-play-circle"></i>
         </span>
       </button>
-      <a href="../home" class="active">
-        <span class="material-symbols-sharp">house</span>
-        <h1 class="h1">Home</h1>
-      </a>
-      <a href="#">
-        <span class="material-symbols-sharp">search</span>
-        <h1 class="h1">Search</h1>
-      </a>
-      <a href="../friends">
-        <span class="material-symbols-sharp">person_add</span>
-        <h1 class="h1">Friends</h1>
-      </a>
-      <a href="../games">
-        <span class="material-symbols-sharp">gamepad</span>
-        <h1 class="h1">Games</h1>
-      </a>
-      <a href="../collections">
-        <span class="material-symbols-sharp">gamepad</span>
-        <h1 class="h1">Collections</h1>
-      </a>
-      <a href="../notifications">
-        <span class="material-symbols-sharp">mail_outline</span>
-        <h1 class="h1">Notifications</h1>
-        <circle class="message-count">0</circle>
-      </a>
-      <a href="../trophy">
-        <span class="material-symbols-sharp">star</span>
-        <h1 class="h1">Trophy</h1>
-      </a>
-      <a href="../tchat">
-        <span class="material-symbols-sharp">person</span>
-        <h1 class="h1">Tchat</h1>
-      </a>
-      <a href="../streams">
-        <span class="material-symbols-sharp">camera</span>
-        <h1 class="h1">Streams</h1>
-      </a>
-      <a href="../balance">
-        <span class="material-symbols-sharp">add</span>
-        <h1 class="h1">Balance</h1>
-      </a>
-      <a href="../suggests">
-        <span class="material-symbols-sharp">add</span>
-        <h1 class="h1">Suggestions</h1>
-      </a>
-	  <a href="../downloads">
-        <span class="material-symbols-sharp">download</span>
-        <h1 class="h1">Downloads</h1>
-      </a>
-      <div class="icons">
-        <i class="fa-brands fa-twitch"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-youtube"></i>
-        <i class="fa-brands fa-discord"></i>
-      </div>
-      <a href="#">
-        <span class="material-symbols-sharp">logout</span>
-        <h1 class="h1">Support</h1>
-      </a>
-      <a href="#">
-        <span class="material-symbols-sharp">settings</span>
-        <h1 class="h1">Settings</h1>
-      </a>
-      <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-        <span class="material-symbols-sharp">logout</span>
-        <h1 class="h1">{{ ('Logout') }}</h1>
-      </a>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none"> @csrf </form>
+    </a>
+    <a href="../home" class="active">
+      <span class="material-symbols-sharp">house</span>
+      <h1 class="h1">Home</h1>
+    </a>
+    <a href="#">
+      <span class="material-symbols-sharp">search</span>
+      <h1 class="h1">Search</h1>
+    </a>
+    <a href="../friends">
+      <span class="material-symbols-sharp">person_add</span>
+      <h1 class="h1">Friends</h1>
+    </a>
+    <a href="../games">
+      <span class="material-symbols-sharp">gamepad</span>
+      <h1 class="h1">Games</h1>
+    </a>
+    <a href="../collections">
+      <span class="material-symbols-sharp">gamepad</span>
+      <h1 class="h1">Collections</h1>
+    </a>
+    <a href="../notifications">
+      <span class="material-symbols-sharp">mail_outline</span>
+      <h1 class="h1">Notifications</h1>
+      <circle class="message-count">0</circle>
+    </a>
+    <a href="../trophy">
+      <span class="material-symbols-sharp">star</span>
+      <h1 class="h1">Trophy</h1>
+    </a>
+    <a href="../tchat">
+      <span class="material-symbols-sharp">person</span>
+      <h1 class="h1">Tchat</h1>
+    </a>
+    <a href="../streams">
+      <span class="material-symbols-sharp">camera</span>
+      <h1 class="h1">Streams</h1>
+    </a>
+    <a href="../balance">
+      <span class="material-symbols-sharp">add</span>
+      <h1 class="h1">Balance</h1>
+    </a>
+    <a href="../suggests">
+      <span class="material-symbols-sharp">add</span>
+      <h1 class="h1">Suggestions</h1>
+    </a>
+    <a href="../downloads">
+      <span class="material-symbols-sharp">download</span>
+      <h1 class="h1">Downloads</h1>
+    </a>
+    <div class="icons">
+      <i class="fa-brands fa-twitch"></i>
+      <i class="fa-brands fa-twitter"></i>
+      <i class="fa-brands fa-instagram"></i>
+      <i class="fa-brands fa-facebook"></i>
+      <i class="fa-brands fa-youtube"></i>
+      <i class="fa-brands fa-discord"></i>
     </div>
-  </aside>
+    <a href="#">
+      <span class="material-symbols-sharp">logout</span>
+      <h1 class="h1">Support</h1>
+    </a>
+    <a href="#">
+      <span class="material-symbols-sharp">settings</span>
+      <h1 class="h1">Settings</h1>
+    </a>
+    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+      <span class="material-symbols-sharp">logout</span>
+      <h1 class="h1">{{ ('Logout') }}</h1>
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none"> @csrf </form>
+  </div>
+</aside>
