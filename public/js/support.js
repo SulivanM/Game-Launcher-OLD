44c9af3873ee67fdbef87900655e35ba070c4788
Sulivan MICHEL <sulivan.michel@switchcompagnie.eu>
2024-01-17 19:01:37 +0100

    JS Support Fix FAQ

--- public/js/support.js
+++ public/js/support.js
@@ -15,3 +15,19 @@ function showFAQ() {
     document.getElementById("formSection").style.display = "none";
     document.getElementById("faqSection").style.display = "block";
 }
+
+function toggleAnswer(index) {
+    var faqAnswer = document.getElementsByClassName('faq-answer');
+    faqAnswer[index].style.display = (faqAnswer[index].style.display === 'block') ? 'none' : 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var faqItems = document.getElementsByClassName('faq-item');
+    for (var i = 0; i < faqItems.length; i++) {
+        faqItems[i].addEventListener('click', function () {
+            this.classList.toggle('active');
+            var index = Array.prototype.indexOf.call(this.parentElement.children, this);
+            toggleAnswer(index);
+        });
+    }
+});
