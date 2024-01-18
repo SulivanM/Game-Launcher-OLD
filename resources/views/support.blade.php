94ad34774613522442fa00efef9387b1a4c8524c
Sulivan MICHEL <sulivan.michel@switchcompagnie.eu>
2024-01-18 19:07:00 +0100

    Fix Ticket Session

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -29,9 +29,25 @@
     </div>
     <div class="title-space" id="ticketsSection">
       <h2>Support Tickets</h2>
+      @if(count($user->tickets) > 0)
+      @foreach($user->tickets as $ticket)
+      <div>
+        <h3>{{ $ticket->subject }}</h3>
+        <p>{{ $ticket->description }}</p>
+        <p>Status: {{ $ticket->status }}</p>
 
-      {{ dd($user) }}
-
+        @if(Auth::user() && (Auth::user()->id === $ticket->user_id) && $ticket->status !== 'closed')
+        <form action="{{ route('tickets.close', ['ticket' => $ticket]) }}" method="POST">
+          @csrf
+          @method('PATCH')
+          <button type="submit">Close Ticket</button>
+        </form>
+        @endif
+      </div>
+      @endforeach
+      @else
+      <p>You dont have ticket for the moment.</p>
+      @endif
     </div>
     <div class="title-space" id="formSection" style="display: none;">
       <h2>Support Tickets</h2>
