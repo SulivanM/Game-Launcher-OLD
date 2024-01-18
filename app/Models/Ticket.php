ce5ee6514eea08c9e2e33a0a122020b6d7a29ccc
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-01-18 13:55:46 +0100

    Update Model

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Ticket extends Mode
+class Ticket extends Model
 {
     protected $fillable = ['user_id', 'subject', 'description', 'status'];
 
