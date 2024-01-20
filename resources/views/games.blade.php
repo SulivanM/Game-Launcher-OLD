76f53e37412d4e776f979c2d1dfb93af7322c304
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-01-20 18:07:08 +0100

    Test Fix View Games

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -47,12 +47,13 @@
                                 <h1>{{ $game->subtitle }} <span class="primary">!</span></h1>
                                 <p>{{ $game->description }}</p>
                                 <div class="button">
+                                    <form action="{{ route('collections.add', ['gameId' => $game->id]) }}"
+                                        method="post">
+                                        @csrf
+                                        <button type="submit" class="strim-btn">ADD TO MY COLLECTION</button>
+                                    </form>
                                     <a href="{{ $game->getGameRoute() }}" class="strim-btn">PLAY NOW</a>
                                 </div>
-                                <form action="{{ route('collections.add', ['gameId' => $game->id]) }}" method="post">
-                                    @csrf
-                                    <button type="submit" class="strim-btn">ADD TO MY COLLECTION</button>
-                                </form>
                             </div>
                         </div>
                     </div>
