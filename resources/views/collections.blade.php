4c2809378b21278a796f3093ac02c7bba16dcca6
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-01-20 17:21:43 +0100

    View Collections Test Update

--- resources/views/collections.blade.php
+++ resources/views/collections.blade.php
@@ -3,7 +3,15 @@
 <div class="container">
   @include('partials.sidebar')
   <main>
-   @include('partials.navbar')
+    @include('partials.navbar')
+    <div class="all-games-container">
+      @foreach ($games as $game)
+      <div class="all-games">
+        <h2>{{ $game->title }}</h2>
+        <p>{{ $game->description }}</p>
+      </div>
+      @endforeach
+    </div>
   </main>
 </div>
 <script src="{{ asset('js/index.js') }}"></script>
