7047b9b6952483cc8a8ac738536c32e4c16c6495
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-01-21 12:20:02 +0100

    Add Coin Test

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,6 +19,7 @@
       <div class="profile">
         <div class="info">
           <p><a href="{{ route('profile') }}">{{ Auth::user()->name }}</a></p>
+          <p class="coin-text">0.00 DCOIN</p>
         </div>
         <a href="{{ route('profile') }}">
           <div class="profile-photo">
