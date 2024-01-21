7cfbcbae7a5620e14fbd971fe388d32485495bcf
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-01-21 12:42:43 +0100

    Fix Migration

--- database/migrations/2024_01_20_155041_rename_balance_column_to_dcoin_in_users_table.php
+++ database/migrations/2024_01_20_155041_rename_balance_column_to_dcoin_in_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddBalanceToUsersTable extends Migration
+class RenameBalanceColumnToDcoinInUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddBalanceToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('balance');
+            $table->renameColumn('balance', 'dcoin');
         });
     }
 
@@ -26,7 +26,7 @@ class AddBalanceToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->decimal('balance', 8, 2)->default(0.00)->after('language');
+            $table->renameColumn('dcoin', 'balance');
         });
     }
 }
