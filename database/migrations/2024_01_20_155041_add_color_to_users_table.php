4ca0b594125d92d2aa247bf053b563c51257fa0e
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-02-01 17:33:05 +0100

    Add Migration Futur Feat Color Custom

--- database/migrations/2024_01_20_155041_add_color_to_users_table.php
+++ database/migrations/2024_01_20_155041_add_color_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameBalanceColumnToDcoinInUsersTable extends Migration
+class AddColorToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class RenameBalanceColumnToDcoinInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('balance', 'dcoin');
+            $table->string('color')->default('#a3bdcc')->after('dcoin');
         });
     }
 
@@ -26,7 +26,7 @@ class RenameBalanceColumnToDcoinInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('dcoin', 'balance');
+            $table->dropColumn('color');
         });
     }
 }
