1489b48011cdddcfaaaa9e66929f73579991a4dc
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-02-01 19:13:21 +0100

    Test Settings Update

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app') @section('content')
+@extends('layouts.app') 
+@section('content')
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <div class="container">
   @include('partials.sidebar')
@@ -15,44 +16,19 @@
           @method('POST')
           <label for="color">Color Customisation</label>
           <input type="text" id="colorPicker" name="color" data-coloris>
-          <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-
-      </div>
-      <div class="settings-box">
-        <h1>Profile</h1>
-        <form method="POST" action="{{ route('settings.update') }}">
-          @csrf
-          @method('POST')
-
-          <div class="settings-form-group">
-            <label for="prenom">First Name</label>
-            <input type="text" class="form-control" id="prenom" name="prenom" value="{{ $user->prenom }}">
-          </div>
-
-          <div class="settings-form-group">
-            <label for="nom">Last Name</label>
-            <input type="text" class="form-control" id="nom" name="nom" value="{{ $user->nom }}">
-          </div>
-          <label for="color">Color Customisation</label>
-          <input type="text" id="colorPicker" name="color" data-coloris>
-          <div class="settings-form-group">
-            <label for="language">Nationality</label>
-            <select class="form-control" id="language" name="language">
-              <option value="French" @if($user->language == 'French') selected @endif>French</option>
-              <option value="English" @if($user->language == 'English') selected @endif>English</option>
-              <option value="Spanish" @if($user->language == 'Spanish') selected @endif>Spanish</option>
-              <option value="German" @if($user->language == 'German') selected @endif>German</option>
-              <option value="Chinese" @if($user->language == 'Chinese') selected @endif>Chinese</option>
-              <option value="Japanese" @if($user->language == 'Japanese') selected @endif>Japanese</option>
-              <option value="Korean" @if($user->language == 'Korean') selected @endif>Korean</option>
-              <option value="Russian" @if($user->language == 'Russian') selected @endif>Russian</option>
-              <option value="Italian" @if($user->language == 'Italian') selected @endif>Italian</option>
-              <option value="Portuguese" @if($user->language == 'Portuguese') selected @endif>Portuguese</option>
-              <option value="Dutch" @if($user->language == 'Dutch') selected @endif>Dutch</option>
-              <option value="Arabic" @if($user->language == 'Arabic') selected @endif>Arabic</option>
-            </select>
-          </div>
+          
+          <label for="prenom">First Name</label>
+          <input type="text" class="form-control" id="prenom" name="prenom" value="{{ $user->prenom }}">
+          
+          <label for="nom">Last Name</label>
+          <input type="text" class="form-control" id="nom" name="nom" value="{{ $user->nom }}">
+          
+          <label for="language">Nationality</label>
+          <select class="form-control" id="language" name="language">
+            <option value="French" @if($user->language == 'French') selected @endif>French</option>
+            <option value="English" @if($user->language == 'English') selected @endif>English</option>
+            <!-- autres options de langue -->
+          </select>
 
           <button type="submit" class="btn btn-primary">Save</button>
         </form>
