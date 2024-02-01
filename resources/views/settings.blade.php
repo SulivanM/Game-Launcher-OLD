fc5f9e0d8d0baff86d6a834a0138d8981efbfebc
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-02-01 19:15:38 +0100

    Test Settings Color Update

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 @section('content')
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <div class="container">
@@ -8,32 +8,37 @@
     <div class="title-space">
       <h2>Settings</h2>
     </div>
-    <div class="settings-container">
-      <div class="settings-box">
-        <h1>Launcher</h1>
-        <form method="POST" action="{{ route('settings.update') }}">
-          @csrf
-          @method('POST')
+    <form method="POST" action="{{ route('settings.update') }}">
+      @csrf
+      @method('POST')
+      <div class="settings-container">
+        <div class="settings-box">
+          <h1>Launcher</h1>
+          <input type="hidden" name="setting_type[]" value="launcher"> <!-- Add a hidden field to differentiate settings -->
           <label for="color">Color Customisation</label>
           <input type="text" id="colorPicker" name="color" data-coloris>
-          
-          <label for="prenom">First Name</label>
-          <input type="text" class="form-control" id="prenom" name="prenom" value="{{ $user->prenom }}">
-          
-          <label for="nom">Last Name</label>
-          <input type="text" class="form-control" id="nom" name="nom" value="{{ $user->nom }}">
-          
-          <label for="language">Nationality</label>
-          <select class="form-control" id="language" name="language">
-            <option value="French" @if($user->language == 'French') selected @endif>French</option>
-            <option value="English" @if($user->language == 'English') selected @endif>English</option>
-            <!-- autres options de langue -->
-          </select>
-
-          <button type="submit" class="btn btn-primary">Save</button>
-        </form>
+        </div>
+        <div class="settings-box">
+          <h1>Profile</h1>
+          <input type="hidden" name="setting_type[]" value="profile"> <!-- Add a hidden field to differentiate settings -->
+          <div class="settings-form-group">
+            <label for="prenom">First Name</label>
+            <input type="text" class="form-control" id="prenom" name="prenom" value="{{ $user->prenom }}">
+          </div>
+          <div class="settings-form-group">
+            <label for="nom">Last Name</label>
+            <input type="text" class="form-control" id="nom" name="nom" value="{{ $user->nom }}">
+          </div>
+          <div class="settings-form-group">
+            <label for="language">Nationality</label>
+            <select class="form-control" id="language" name="language">
+              <!-- Options -->
+            </select>
+          </div>
+        </div>
       </div>
-    </div>
+      <button type="submit" class="btn btn-primary">Save</button>
+    </form>
   </main>
 </div>
 <script src="{{ asset('js/index.js') }}"></script>
