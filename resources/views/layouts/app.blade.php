4b41239b04cb42b9cc15c029aa6d81a48ff5f7a8
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-02-02 18:51:29 +0100

    Customisation To File External

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,60 +79,7 @@
           </ul>
         </div>
   </div>
-  <style>
-    .navbar sup {
-        background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .play-button {
-        background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .aside .sidebar a.active {
-        color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .aside .sidebar a .active span {
-        color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    aside .sidebar .message-count {
-        background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .active {
-        background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .primary {
-        color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .line {
-        border-bottom: 4px solid {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .strim-btn {
-        background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .launcher-dl-button {
-        border: 3px solid {{ optional($user)->color ?? '#FFFFFF' }};
-        box-shadow: 5px 5px {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .launcher-dl-button:active {
-        box-shadow: 0px 0px {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .card button {
-      background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-
-    .button-all-games {
-      background-color: {{ optional($user)->color ?? '#FFFFFF' }};
-    }
-</style>
+  @include('partials.customisation')
   <main>@yield('content')</main>
   </div>
   @include('partials.footer')
---
+++ resources/views/partials/customisation.blade.php
@@ -0,0 +1,54 @@
+<style>
+   .navbar sup {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .play-button {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .aside .sidebar a.active {
+   color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .aside .sidebar a .active span {
+   color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   aside .sidebar .message-count {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .active {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .primary {
+   color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .line {
+   border-bottom: 4px solid {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .strim-btn {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .launcher-dl-button {
+   border: 3px solid {{ optional($user)->color ?? '#FFFFFF' }};
+   box-shadow: 5px 5px {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .launcher-dl-button:active {
+   box-shadow: 0px 0px {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+   
+   .card button {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+
+   .button-all-games {
+   background-color: {{ optional($user)->color ?? '#FFFFFF' }};
+   }
+</style>
