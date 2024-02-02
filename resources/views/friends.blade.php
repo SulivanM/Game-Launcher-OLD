478fa403eafa5f1d86a3bbdd9abcdac2230aebf9
SulivanM <sulivan.michel@switchcompagnie.eu>
2024-02-02 22:27:03 +0100

    Test Friends View Fix

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -33,6 +33,7 @@
         <ul>
             @foreach($users as $user)
             <li>
+                {{ $user->name }}
                 <form method="POST" action="{{ route('send.friend.request') }}"
                     id="sendFriendRequestForm{{ $user->id }}">
                     @csrf
@@ -41,6 +42,26 @@
             </li>
             @endforeach
         </ul>
+
+        <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        @foreach($users as $user)
+        const userName{{ $user->id }} = "{{ $user->name }}";
+
+        Swal.fire({
+            title: 'Send Friend Request',
+            text: `Are you sure to send a friend request to ${userName{{ $user->id }}}`,
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Yes !',
+            cancelButtonText: 'No',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById('sendFriendRequestForm{{ $user->id }}').submit();
+            }
+        });
+        @endforeach
+        </script>
         <div class="section" id="my-friends" style="display:none;">
             <h2>Friends</h2>
             @if($friends->isEmpty())
@@ -70,24 +91,4 @@
 </div>
 <script src="{{ asset('js/index.js') }}"></script>
 <script src="{{ asset('js/friends.js') }}"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        @foreach($users as $user)
-        const userName{{ $user->id }} = "{{ $user->name }}";
-
-        Swal.fire({
-            title: 'Send Friend Request',
-            text: `Are you sure you want to send a friend request to ${userName{{ $user->id }}}?`,
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonText: 'Yes !',
-            cancelButtonText: 'No',
-        }).then((result) => {
-            if (result.isConfirmed) {
-                document.getElementById('sendFriendRequestForm{{ $user->id }}').submit();
-            }
-        });
-        @endforeach
-    });
-</script>
 @endsection
